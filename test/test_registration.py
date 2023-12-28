3ade987707e318bd46822ebcaeff6d8a1875f806
Oleg <kov2905@icloud.com>
2023-12-28 18:03:04 +0200

    add import

--- test/test_registration.py
+++ test/test_registration.py
@@ -1,13 +1,15 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
 import allure
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 from locators import Locators
+from test_date.user_data import UserData
 
 
 class TestRegistrationPass:
     @allure.feature('Регистрация')
-    def test_registration(self, driver, user_data):
+    def test_registration(self, driver: WebDriver, user_data: UserData):
         driver.find_element(*Locators.AUTH_BUTTON).click()
         driver.find_element(*Locators.REG_BUTTON).click()
         driver.find_element(*Locators.NAME_INPUT).send_keys(user_data.name)
